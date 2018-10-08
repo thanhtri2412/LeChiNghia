621b374cdaa1ac2968b3c0488a440f1d71292770
phonglu20 <43948217+phonglu20@users.noreply.github.com>
2018-10-08 16:06:57 +0700

    as
    sd

--- source.cpp
+++ source.cpp
@@ -1,26 +1,4 @@
 int USCLN(int a, int b)
 {
-	a = abs(a); 
-	b = abs(b);  
-	if (a == 0 && b != 0)
-	{
-		return b;
-	}
-	else if (a != 0 && b == 0)
-	{
-		return a;
-	}
-	while (a != b)
-	{
-		if (a > b)
-		{
-			a -= b;
-		}
-		else
-		{
-			b -= a;
-		}
-	}
-	return a;
-
+	40
 }
